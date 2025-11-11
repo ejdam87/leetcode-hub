0f0ae2626161ba42500d1ef99c25508d7453f3ac
ejdam87 <adam.dzadon@gmail.com>
2025-11-11 14:38:55 +0100

    feat: 3D dp

--- medium/ones.py
+++ medium/ones.py
@@ -3,33 +3,6 @@ from typing import List
 
 class Solution:
 
-    def _find_rec(
-            self,
-            strs: list[str],
-            counts: dict[str, tuple[int, int]],
-            i: int,
-            current_zeros: int,
-            current_ones: int) -> int:
-
-        if current_zeros < 0 or current_ones < 0:
-            return 0
-
-        if i >= len(strs):
-            return 0
-
-        z, o = counts[strs[i]]
-
-        # include
-        if (current_zeros - z < 0) or ( current_ones - o < 0 ):
-            a = 0
-        else:
-            a = 1 + self._find_rec( strs, counts, i + 1, current_zeros - z, current_ones - o )
-
-        # not include
-        b = self._find_rec( strs, counts, i + 1, current_zeros, current_ones )
-
-        return max(a, b)
-
 
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         counts = {}
@@ -38,7 +11,23 @@ class Solution:
             ones = s.count("1")
             counts[s] = (zeros, ones)
 
-        return self._find_rec( strs, counts, 0, m, n )
+        dp = [ [ [ 0 for no in range(n + 1) ] for nz in range(m + 1)]  for i in range( len(strs) + 1 )]
 
+        for i in range( 1, len(strs) + 1 ):
+            nz, no = counts[ strs[i - 1] ]
+            for z in range(m + 1):
+                for o in range(n + 1):
+                    # take
+                    if z - nz < 0 or o - no < 0:
+                        t = 0
+                    else:
+                        t = dp[i - 1][z - nz][o - no] + 1
 
-print( Solution().findMaxForm( ["00011","00001","00001","0011","111"], 8, 5 ) )
+                    # not take
+                    nt = dp[i - 1][z][o]
+
+                    dp[i][z][o] = max(t, nt)
+
+        return dp[-1][-1][-1]
+
+print( Solution().findMaxForm( ["10","0001","111001","1","0"], 5, 3 ) )
